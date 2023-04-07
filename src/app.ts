53f47e82befdc9b7ea2364c7c997e61c46b91979
Anshul Kulkarni <anshul.kulkarni@hyperverge.co>
2023-04-07 17:58:41 -0400

    completed integration of part 1

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import { dependency } from './dependencyMetric';
 import { pullRequestRating } from './pullRequestMetric'
-
+import { getMetrics } from './part1handler';
 
 
 // Connect to MongoDB Atlas cluster
@@ -30,6 +30,8 @@ opts = {
         };
     },
 }
+
+
 const storage = new GridFSStorage.GridFsStorage(opts);
 const upload = multer({ storage: storage }); 
 
@@ -44,6 +46,7 @@ app.get('/directory', async (req, res) => {
     try {
         const collection = db.collection('uploads.files');
         const files = await collection.find().toArray();
+
 
         // Needs to be in a loop 
         const dependencyMetric = await dependency("nullivex", "nodist", "3.6.2")
@@ -62,8 +65,57 @@ app.get('/upload', (req, res) => {
     res.render('upload');
 });
 
-app.post('/upload', upload.array('zipfile'), (req, res) => {
+app.post('/upload', upload.array('zipfile'), async (req, res) => {
+    const githubURL = req.body.githubURL
+    const result = extractOwnerAndRepo(githubURL)
+    const owner = result?.owner ?? ''
+    const repo = result?.repo ?? ''
+
+    const collection = db.collection('uploads.files');
+    const files = await collection.find().toArray();
+    const file = files[files.length-1]
+
+    // const dependencyMetric = await dependency(owner, repo, "3.6.2")
+    const dependencyMetric = -1
+    // const pullRequestMetric = await pullRequestRating(owner, repo)
+    const pullRequestMetric = -1
+
+
+    const part1Metrics = await getMetrics(githubURL)
+    const netScore = part1Metrics.netScore
+    const rampUpScore = part1Metrics.rampUpScore
+    const correctnessScore = part1Metrics.correctnessScore
+    const busFactorScore = part1Metrics.busFactorScore
+    const respScore = part1Metrics.respScore
+    const licenseScore = part1Metrics.licenseScore
+
+
+
+    const filter = { _id: file._id };
+    // const ownerUpdate = { $set: { owner: owner} };
+    // const repoUpdate = { $set: { repo: repo } };
+    // const dependencyUpdate = { $set: { dependencyMetric: dependencyMetric} };
+    // const pullRequestUpdate = { $set: { pullRequestMetric: pullRequestMetric} };
+    
+
+    const update = {$set: {owner: owner, repo: repo, netScore: netScore, rampUpScore: rampUpScore, 
+        correctnessScore: correctnessScore, busFactorScore: busFactorScore, respScore: respScore, 
+        licenseScore: licenseScore, dependencyMetric: dependencyMetric, pullRequestMetric: pullRequestMetric}}
+
+        await collection.updateOne(filter, update)
+
+    // await collection.updateOne(filter, ownerUpdate)
+    // await collection.updateOne(filter, repoUpdate)
+    // await collection.updateOne(filter, dependencyUpdate)
+    // await collection.updateOne(filter, pullRequestUpdate)
+
+
     res.send('Upload worked')
+});
+
+app.get('/reset', async (req, res) => {
+    await bucket.drop()
+    res.send("Reset Complete")
 });
 
 
@@ -74,6 +126,23 @@ app.get('/download/:filename', async (req, res) => {
   
 });
 
+function extractOwnerAndRepo(githubLink: string): { owner: string; repo: string } | null {
+    const parts = githubLink.split('/');
+    const ownerIndex = parts.indexOf('github.com') + 1;
+  
+    if (ownerIndex < 2 || ownerIndex >= parts.length - 1) {
+        return null;
+    }
+
+    const owner = parts[ownerIndex];
+    const repo = parts[ownerIndex + 1].replace('.git', '');
+  
+    return { owner, repo };
+  }
+
+
+
+  
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
