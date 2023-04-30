8e898196cb405099a1302d7f33f9ead5af06a11e
Nidhi Kunigal <70180635+nidhikunigal@users.noreply.github.com>
2023-04-29 21:05:26 -0400

    Update dependencyMetric.ts

--- src/metrics/dependencyMetric.ts
+++ src/metrics/dependencyMetric.ts
@@ -1,125 +1,3 @@
-// import { Octokit} from "@octokit/rest";
-// import * as dotenv from 'dotenv'
-// dotenv.config()
-
-// const octokit = new Octokit({
-//   auth: process.env.GITHUB_TOKEN,
-//   version: "latest",
-// });
-
-// export async function dependency(owner: string, repo: string) {
-//   try {
-//     const response = await octokit.repos.getContent({
-//       owner,
-//       repo,
-//       path: "package.json"
-//     });
-//   //console.log(response.data)
-//   const responseJSON = JSON.parse(JSON.stringify(response.data))
-//   const packageJson = JSON.parse(Buffer.from(responseJSON.content, 'base64').toString('utf8'))
-//   const dependencies = packageJson.dependencies;
-
-//   let notPinned = 0;
-//   let pinned = 0;
-//   for (const [dependencyName, version] of Object.entries(dependencies)) {
-//     const versionJSON = JSON.parse(JSON.stringify(version))
-//     const [large, med, small] = versionJSON.version.toString().split('.')
-//     const finder = versionJSON.toString().indexOf("-")
-//     const carrot = versionJSON.toString().indexOf("^")
-
-//     //if it is in form "1"
-//     if ((med == undefined) && (small == undefined)){
-//       notPinned++
-//     }
-//     //if it is in form 1.0-3.2
-//     else if (finder != -1 ){
-//       notPinned++
-//     }
-//     //if you find a carrot 
-//     else if (carrot != -1){
-//       //major version pinned is unacceptable with a carrot (ex. ^2.1 is not okay, but ^0.x is okay)
-//       if (large != undefined){
-//         notPinned++
-//       }
-//     }
-//     //otherwise it is in an acceptable format
-//     else {
-//       pinned++
-//     }
-//   }
-
-//   //total num of dependencies
-//   const numberOfDependencies = Object.keys(dependencies).length;
-//   // console.log(`counter = ${counter}`)
-//   // console.log(`The Git repository has ${numberOfDependencies} dependencies.`);
-//   //
-//     if (numberOfDependencies == 0) {
-//       return 1.0
-//     } else {
-//       const val = pinned / numberOfDependencies
-//       return val
-//     }
-
-//   }
-//   catch(error) {
-//     const resp1 = await octokit.repos.getContent({
-//       owner,
-//       repo,
-//       path: "package-lock.json"
-//     });
-//     const responseJSON = JSON.parse(JSON.stringify(resp1.data))
-//     const packageJson = JSON.parse(Buffer.from(responseJSON.content, 'base64').toString('utf8'))
-
-//     const dependencies = packageJson.dependencies;
-
-
-//     let notPinned = 0
-//     let pinned = 0
-
-//     for (const version of Object.entries(dependencies)) {
-//       //finding out if it is larger than pinned dependency
-//       const versionJSON = JSON.parse(JSON.stringify(version))
-//       const [large, med, small] = versionJSON.version.toString().split('.')
-//       const finder = versionJSON.toString().indexOf("-")
-//       const carrot = versionJSON.toString().indexOf("^")
-  
-
-//       //if it is in form "1"
-//       if ((med == undefined) && (small == undefined)){
-//         notPinned++
-//       }
-//       //if it is in form 1.0-3.2
-//       else if (finder != -1 ){
-//         notPinned++
-//       }
-//       //if you find a carrot 
-//       else if (carrot != -1){
-//         //major version pinned is unacceptable with a carrot (ex. ^2.1 is not okay, but ^0.x is okay)
-//         if (large != undefined){
-//           notPinned++
-//         }
-
-//       }
-//       //otherwise it is in an acceptable format
-//       else {
-//         pinned++
-//       }
-//     }
-
-//       //total num of dependencies
-//       const numberOfDependencies = Object.keys(dependencies).length;
-
-//         if (numberOfDependencies == 0) {
-//           return 0.0
-//         } else {
-//           const val = pinned / numberOfDependencies
-//           return val
-//         }
-//     }
-
-// }
-
-
 import { Octokit} from "@octokit/rest";
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -140,12 +18,13 @@ export async function dependency(owner: string, repo: string) {
   const responseJSON = JSON.parse(JSON.stringify(response.data))
   const packageJson = JSON.parse(Buffer.from(responseJSON.content, 'base64').toString('utf8'))
   const dependencies = packageJson.dependencies;
+  
 
   let notPinned = 0;
   let pinned = 0;
   for (const [dependencyName, version] of Object.entries(dependencies)) {
     const versionJSON = JSON.parse(JSON.stringify(version))
-    const [large, med, small] = versionJSON.version.toString().split('.')
+    const [large, med, small] = versionJSON.toString().split('.')
     const finder = versionJSON.toString().indexOf("-")
     const carrot = versionJSON.toString().indexOf("^")
 
@@ -172,17 +51,18 @@ export async function dependency(owner: string, repo: string) {
 
   //total num of dependencies
   const numberOfDependencies = Object.keys(dependencies).length;
-  // console.log(`counter = ${counter}`)
-  // console.log(`The Git repository has ${numberOfDependencies} dependencies.`);
-  //
-    if (numberOfDependencies == 0) {
+
+  if (numberOfDependencies == 0) {
       return 1.0
-    } else {
+      
+  } else {
       const val = pinned / numberOfDependencies
       return val
-    }
 
+    }
+    
   }
+
   catch(error) {
     try {
       const resp1 = await octokit.repos.getContent({
@@ -202,7 +82,7 @@ export async function dependency(owner: string, repo: string) {
       for (const version of Object.entries(dependencies)) {
         //finding out if it is larger than pinned dependency
         const versionJSON = JSON.parse(JSON.stringify(version))
-        const [large, med, small] = versionJSON.version.toString().split('.')
+        const [large, med, small] = versionJSON.toString().split('.')
         const finder = versionJSON.toString().indexOf("-")
         const carrot = versionJSON.toString().indexOf("^")
     
@@ -233,9 +113,11 @@ export async function dependency(owner: string, repo: string) {
         const numberOfDependencies = Object.keys(dependencies).length;
   
           if (numberOfDependencies == 0) {
+
             return 0.0
           } else {
             const val = pinned / numberOfDependencies
+
             return val
           }
       }
